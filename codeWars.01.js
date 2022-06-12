8c6764b6b0f5ff3f3d473480c08c5ff86e343741
Kirill <apatski94@gmail.com>
2022-06-12 21:32:58 +0300

    task_01 add symbol

--- codeWars.01.js
+++ codeWars.01.js
@@ -1,6 +1,6 @@
 function sayHello( name, city, state ) {
     let userName = name.reduce((e, i) => e + ' ' + i);
-    return `Hello, ${userName}! Welcome to ${city}, ${state}`; 
+    return `Hello, ${userName}! Welcome to ${city}, ${state}!`; 
 }
 
 console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'));
